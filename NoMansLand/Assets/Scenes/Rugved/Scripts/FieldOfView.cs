83acc92eb03aacd854eccc40841bad3d5c582afd
Rugved Joshi <joshirugved48@gmail.com>
2024-03-08 21:33:16 -0500

    YE
    YE

---
+++ NoMansLand/Assets/Scenes/Rugved/Scripts/FieldOfView.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                RaycastHit hit;
+
+                // Ensure that the raycast is only checking against the obstructionMask
+                if (Physics.Raycast(transform.position, directionToTarget, out hit, distanceToTarget, obstructionMask))
+                {
+                    if (hit.transform.CompareTag("ObstacleTag"))
+                    {
+                        canSeePlayer = false;
+                        return;
+                    }
+                }
+
+                canSeePlayer = true;
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+    }
+}
